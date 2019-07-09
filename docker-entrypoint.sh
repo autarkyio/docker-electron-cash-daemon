6d9d2cb892de50d7069ba7fd4d4fd4bcf4028882
explodi <phaser@tubatuba.net>
2019-07-09 12:29:12 -0400

    testnet

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,15 +5,15 @@ set -ex
 # trap 'pkill -TERM -P1; electron-cash daemon stop; exit 0' SIGTERM
 
 # Set config
-electron-cash setconfig rpcuser ${ELECTRUM_USER}
-electron-cash setconfig rpcpassword ${ELECTRUM_PASSWORD}
-electron-cash setconfig rpchost 0.0.0.0
-electron-cash setconfig rpcport 7000
+electron-cash setconfig rpcuser ${ELECTRUM_USER} ${OPTIONS}
+electron-cash setconfig rpcpassword ${ELECTRUM_PASSWORD} ${OPTIONS}
+electron-cash setconfig rpchost 0.0.0.0 ${OPTIONS}
+electron-cash setconfig rpcport 7000 ${OPTIONS}
 
 # XXX: Check load wallet or create
 
 # Run application
-electron-cash daemon start
+electron-cash daemon ${OPTIONS} start
 
 # Wait forever
 while true; do
